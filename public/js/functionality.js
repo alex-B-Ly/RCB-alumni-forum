3daf1166161f66be2841861827f5ed6b3f602897
Alex Ly <alexly@Alexs-MBP.home>
2016-04-02 00:51:05 -0400

    Register route created, users can sign up.

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -1,7 +1,23 @@
 // HOMEPAGE ANGULAR
 var home = angular.module('homepage', []);
 
-home.controller('homepageController', function($scope){
+home.controller('homepageController', function($scope, $http){
 
-  $scope.dog = "woof";
+  $scope.register = function(){
+    $http({
+      method: 'POST',
+      url: '/register',
+      data: {
+        firstName: $scope.registerFirstName,
+        lastName: $scope.registerLastName,
+        password: $scope.registerPassword,
+        email: $scope.registerEmail,
+        section: $scope.registerSection
+      }
+    }).then(function(result){
+      console.log(result);
+      // TODO manipulate result
+    });
+  }
+
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var db = require('./config/connection.js');
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -14,9 +15,12 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.get('*', function(req, res) {
-  res.sendFile(process.cwd() + '/public/homepage.html');
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(process.cwd() + '/public/homepage.html');
+// });
+
+var routes = require('./controllers/router.js');
+app.use('/', routes);
 
 app.listen(PORT, function(){
   console.log('listening on ',PORT);
