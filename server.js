5e7c813c6a7bdaae2ec3247b46a4bba888b9ed2e
alex-B-Ly <abl12582@gmail.com>
2016-04-11 07:03:42 -0400

    Mostly functional. Need to do session stuff and maybe move bcrypt compare into router.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var passport = require('passport');
 var db = require('./config/connection.js');
 var PORT = process.env.PORT || 8080;
 
@@ -11,6 +12,9 @@ app.use(express.static('public'));
 
 app.use(logger('dev'));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
@@ -19,6 +23,8 @@ app.use(bodyParser.urlencoded({
 // ROUTES
 var routes = require('./controllers/router.js');
 app.use('/', routes);
+
+require('./config/passport')(passport);
 
 app.listen(PORT, function(){
   console.log('listening on ',PORT);
