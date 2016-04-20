15f3f0692f76285f6c0f2a0c3ddf06a2f15784c3
alex-B-Ly <abl12582@gmail.com>
2016-04-20 15:35:43 -0400

    table fix

--- public/js/controllers/functionality.js
+++ public/js/controllers/functionality.js
@@ -177,15 +177,8 @@ rcb.controller('profileController', ['$scope', '$http', '$state', '$filter', 'Ng
   });
 
   // TODO Add Github table functionality below
-  $scope.githubTable = new NgTableParams({
-      page: 1,
-      count: 10
-    }, {
-    total: $scope.data.length,
-    getData: function($defer, params) {
-        $scope.data = $scope.users.slice((params.page() - 1) * params.count(), params.page() * params.count());
-        $defer.resolve($scope.data);
-      }  
+  $scope.githubTable = new NgTableParams({}, {
+    getData: function($defer, params) {  
       return $http.get('https://api.github.com/users/' + $scope.githubUsername + '/repos')
       .then(function (response) {
         var filteredData = $filter('filter')(response.data, params.filter());
