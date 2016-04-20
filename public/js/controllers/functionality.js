bf5efbef5424033331644b50a0ee5d3e0ab0c8df
alex-B-Ly <abl12582@gmail.com>
2016-04-19 21:18:01 -0400

    Got rid of socket file. Need to pass socket info from server to client

--- public/js/controllers/functionality.js
+++ public/js/controllers/functionality.js
@@ -1,3 +1,4 @@
+var socket = io('http://localhost:8080');
 // HOMEPAGE ANGULAR
 var rcb = angular.module('RCBmessenger');
 
@@ -86,6 +87,7 @@ rcb.controller('sidebarController', ['$scope', '$http', '$state', function($scop
   $scope.sendMessage = function(){
     socket.emit('message', {stuff: $scope.message});
     // TODO Save $scope.message into DB
+
     $scope.message = "";
   }
 
--- public/js/controllers/socket.js
+++
@@ -1,6 +0,0 @@
-var socket = io('http://localhost:8080');
-
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('message', {stuff: 'Here is your stuff.'});
-});
